37fd9cebd8125f1aa177f04e583aa2908cf2b973
Greenvahn <saulverdemartinez@gmail.com>
2021-08-13 19:19:41 +0100

    dayCard.vue centering

--- components/dayCard.vue
+++ components/dayCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <b-card-group deck class="text-center">
-      <div v-for="(day, index) in days" :key="index">
+    <b-card-group class="d-flex flex-row justify-content-center">
+      <div v-for="(day, index) in days" :key="index" class="ml-3 mr-3">
         <b-card
           :header="day"
           header-text-variant="white"
@@ -10,7 +10,7 @@
           :title="`Meal ${index + 1}`"
           style="max-width: 20rem;"
           bg-variant="light"
-          class="mb-4"
+          class="mb-4 text-center"
         >
           <b-card-text>
             With supporting text below as a natural lead-in to additional
