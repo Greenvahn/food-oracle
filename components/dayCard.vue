6e9794b514166f2cd763b04c1947548ae404a6ce
Greenvahn <saulverdemartinez@gmail.com>
2021-08-16 17:27:34 +0100

    implemented basic dropdown to cards

--- components/dayCard.vue
+++ components/dayCard.vue
@@ -7,7 +7,7 @@
           header-text-variant="white"
           header-tag="header"
           header-bg-variant="dark"
-          :title="`Meal ${index + 1}`"
+          :title="cardTitle != null ? cardTitle : `Meal ${index + 1}`"
           style="max-width: 20rem;"
           bg-variant="light"
           class="mb-4 text-center"
@@ -16,9 +16,23 @@
             With supporting text below as a natural lead-in to additional
             content.
           </b-card-text>
-          <b-button href="#" variant="primary">
-            Custom
-          </b-button>
+
+          <div>
+            <b-form>
+              <label
+                class="mr-sm-2"
+                for="inline-form-custom-select-pref"
+              >Preference</label>
+              <b-form-select
+                id="inline-form-custom-select-pref"
+                class="mb-2 mr-sm-2 mb-sm-0"
+                :options="[{ text: 'Choose...', value: null }, ...dropOptions]"
+                :value="null"
+                @change="onChange($event)"
+              />
+            </b-form>
+          </div>
+
           <b-button href="#" variant="primary">
             Ramdom
           </b-button>
@@ -28,6 +42,7 @@
   </div>
 </template>
 <script>
+import { reactive, ref } from '@vue/composition-api'
 export default {
   props: {
     days: {
@@ -36,6 +51,17 @@ export default {
         return []
       }
     }
+  },
+  setup (props) {
+    const dropOptions = reactive(['option 1', 'option 2', 'option 3'])
+    const cardTitle = ref(null)
+
+    // const newMap = props.days.map(item => reactive([`cardTitle${item}`]))
+
+    const onChange = (value) => {
+      cardTitle.value = value
+    }
+    return { dropOptions, cardTitle, onChange }
   }
 }
 </script>
