15c501dd06f2b3633b0624e62b0662f66dc6bce4
Greenvahn <saulverdemartinez@gmail.com>
2021-10-18 14:27:31 +0100

    updated conditional on fetch-data.js

--- utils/fetch-data.js
+++ utils/fetch-data.js
@@ -5,7 +5,7 @@ const getData = async (url, options) => {
     options.response = json
     options.fullCardList = options.response.map(card => card.name) // creates an array with ALL card recipes
 
-    if (process.env.TR_EXCLUDING_CARD.length > 0) {
+    if (process.env.TR_EXCLUDING_CARD && process.env.TR_EXCLUDING_CARD.length > 0) {
       options.recipesList = options.response.reduce((result, card) => { // creates an array excluding card recipes are not included in the food oracle.
         if (!card.name.includes(process.env.TR_EXCLUDING_CARD)) {
           result.push(card.name)
